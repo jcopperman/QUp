e39eb242c7eb4e014b00a55922612450c1570b7b
jcopperman <opperman.jonathan@gmail.com>
2016-09-12 07:16:49 +0200

    Repos

---
+++ QUp.DataModel/DAL/IFeatureRepository.cs
@@ -0,0 +1,19 @@
+﻿using QUp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUp.DataModel.DAL
+{
+    public interface IFeatureRepository
+    {
+        IEnumerable<Feature> GetFeatures();
+        Project GetFeatureByID(int featureId);
+        void InsertFeaturet(Feature feature);
+        void DeleteFeature(int featureId);
+        bool UpdateFeature(Feature feature);
+        bool Save();
+    }
+}
---
+++ QUp.DataModel/DAL/IUserStoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using QUp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUp.DataModel.DAL
+{
+    public interface IUserStoryRepository
+    {
+        IEnumerable<UserStory> GetUserStories();
+        Project GetUserStoryByID(int UserStoryId);
+        void InsertUserStory(UserStory userstory);
+        void DeleteUserStory(int userStoryId);
+        bool UpdateUserStory(UserStory userStory);
+        bool Save();
+    }
+}
