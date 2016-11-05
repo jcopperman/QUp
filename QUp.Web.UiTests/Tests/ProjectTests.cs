15a73b0e2b619c717f9aa1e2316bd1701f44d90d
jcopperman <opperman.jonathan@gmail.com>
2016-11-05 17:35:52 +0200

    UI Tests project created

---
+++ QUp.Web.UiTests/Tests/ProjectTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUp.Web.UiTests
+{
+    public class ProjectTests
+    {
+    }
+}
